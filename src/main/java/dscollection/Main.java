90310263e138c86a5ce6d96bde7f4bd242217cf5
Abdur740 <123439024+Abdur740@users.noreply.github.com>
2023-03-02 14:53:12 +0530

    Update Main.java

--- src/main/java/dscollection/Main.java
+++ src/main/java/dscollection/Main.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Main {
+protected Main(){}
     private static final Logger log = Logger.getLogger("InfoLogging");
     public static void start() {
         Scanner sc = new Scanner(System.in);
-        boolean times = false;
-        while(times == false){
+        
+        while(true){
             log.info("Choose Options\n1. Array\n2.Linkedlist\n3.Exit");
             int option = sc.nextInt();
         if(option ==1){
@@ -20,7 +21,7 @@ public class Main {
             llexecution.linkedlistexecution();
         }
         else if(option ==3){
-            times = true;
+            return ;
         }
         else{
             log.info("Enter a valid option.");
